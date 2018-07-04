3a4f0bf211791992f96b9439b0812ae14a69a75c
Franziska Paetzold <s0554620@htw-berlin.de>
2018-07-04 18:15:44 +0200

    command game update: nodes movable by moving touch

--- Git(Hub)-Tutorial/Models/CommandGameModel.swift
+++ Git(Hub)-Tutorial/Models/CommandGameModel.swift
@@ -8,19 +8,20 @@
 
 import UIKit
 import SpriteKit
-import CoreMotion
 
 class CommandGameModel: SKScene {
     
     let backgroundNode = SKSpriteNode(imageNamed: "background")
     let foregroundNode = SKSpriteNode()
-    let cargoNode = SKSpriteNode(imageNamed: "cargo")
+    
     let shipNode = SKSpriteNode(imageNamed: "ship")
+    let cargoNode = SKSpriteNode(imageNamed: "cargo")
+    var shippingItems: [SKNode] = []
+    
     let collisionCategoryCargo: UInt32 = 0x1 << 1
     let collisionCategoryShip: UInt32 = 0x1 << 2
     
-    //get acess to iOS motion services
-    let coreMotionManager = CMMotionManager()
+    var touchedNode = SKSpriteNode()
     
     
     required init?(coder aDecoder: NSCoder){
@@ -32,7 +33,6 @@ class CommandGameModel: SKScene {
         super.init(size: size)
         // needed for extension
         physicsWorld.contactDelegate = self
-        physicsWorld.gravity = CGVector(dx: 0.0, dy: -0.1) //TODO: dy: 5.0
         
         //ability for user to apply an impulse
         isUserInteractionEnabled = true
@@ -45,13 +45,15 @@ class CommandGameModel: SKScene {
         backgroundNode.anchorPoint = CGPoint(x: 0.5, y: 0.0) //on which point the node is set (0.5 & 0 is bottom center of the node)
         backgroundNode.position = CGPoint (x: size.width / 2.0, y: 0.0) //set node to the middle and the bottom of the scene
         addChild(backgroundNode) // adds node to scene
+        
         //============add foreground==============
         addChild(foregroundNode)
         
         //============configuration cargo============
         cargoNode.physicsBody = SKPhysicsBody (rectangleOf: CGSize(width: cargoNode.size.width, height: cargoNode.size.height))
+        cargoNode.name = "CARGO"
         cargoNode.physicsBody?.isDynamic = false //false, because the package shouldnt fall out of the screen
-        cargoNode.position = CGPoint(x: size.width/2.0, y: 180.0) // anchorPoint is the middle
+        cargoNode.position = CGPoint(x: size.width/2.0, y: size.height/10*9) // anchorPoint is the middle
         //stop rotation by collision
         cargoNode.physicsBody?.allowsRotation = false
         
@@ -59,10 +61,16 @@ class CommandGameModel: SKScene {
         cargoNode.physicsBody?.contactTestBitMask = collisionCategoryShip
         cargoNode.physicsBody?.collisionBitMask = 0 //handling collision on our own
         foregroundNode.addChild(cargoNode)
+        shippingItems.append(cargoNode)
         
         //============configuration ship============
         shipNode.name = "SHIP"
-        shipNode.position = CGPoint(x: cargoNode.position.x + 20, y: cargoNode.position.y)
+        shipNode.anchorPoint = CGPoint(x:1.0, y: 0.0) //anchor ist on the bottom right of the picture
+        shipNode.position = CGPoint(x: (size.width/10)*9.9, y: (size.height/10)*0.1)
+        print("full width: \(size.width)")
+        print("curr width: \((size.width/10)*9.9)")
+        print("full height: \(size.height)")
+        print("curr height: \((size.height/10)*0.1)")
         shipNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: shipNode.size.width, height: shipNode.size.height))
         shipNode.physicsBody?.isDynamic = false //no gravity
             
@@ -75,43 +83,29 @@ class CommandGameModel: SKScene {
     
     //needs isUserInteractionEnabled = true in initializer
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if !cargoNode.physicsBody!.isDynamic {
-            cargoNode.physicsBody?.isDynamic = true
-            
-            coreMotionManager.accelerometerUpdateInterval = 0.3 //3/10th of a second
-            coreMotionManager.startAccelerometerUpdates()
-        }
+       
     }
     
-    //changes the position of the background based on the position of the player
-    override func update(_ currentTime: TimeInterval) {
-        backgroundNode.position = CGPoint(x: backgroundNode.position.x, y: -((cargoNode.position.y - 180.0)/8))
-    }
-    //TODO player can still leave the view
-    
-    override func didSimulatePhysics(){
-        if let accelerometerData = coreMotionManager.accelerometerData {
-            cargoNode.physicsBody!.velocity = CGVector(dx: CGFloat(accelerometerData.acceleration.x * 360), dy: cargoNode.physicsBody!.velocity.dy)
-        }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let touchPosition = touch.location(in: self)
+            let touchedNode = self.atPoint(touchPosition)
         
-        /*
-        if playerNode.position.x < -(playerNode.size.width/2){
-            playerNode.position = CGPoint(x: size.width - playerNode.size.width / 2, y: playerNode.position.y)
+            if shippingItems.contains(touchedNode) {
+                touchedNode.position.x = touchPosition.x
+                touchedNode.position.y = touchPosition.y
+            }
+            
         }
-        else if playerNode.position.x > self.size.width {
-            playerNode.position = CGPoint(x: playerNode.size.width / 2, y: playerNode.position.y)
-        }
-         */
     }
     
-    deinit {
-        coreMotionManager.stopAccelerometerUpdates()
-    }
 }
 
 //remove package node if it contacts the ship
 extension CommandGameModel: SKPhysicsContactDelegate{
     func didBegin(_ contact: SKPhysicsContact) {
+        print("contact")
+        
         //a represents first body in the contact (cargo), b property represents second body
         let contactNode = contact.bodyA.node!
         
