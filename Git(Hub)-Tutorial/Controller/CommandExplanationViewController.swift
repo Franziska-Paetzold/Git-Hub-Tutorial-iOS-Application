7ffb2ae60c08387db9a52f90a71b1668d43c7876
Franziska Paetzold <s0554620@htw-berlin.de>
2018-08-26 12:18:17 +0200

    update CommandExplanantionViewController - working autolayout

--- Git(Hub)-Tutorial/Controller/CommandExplanationViewController.swift
+++ Git(Hub)-Tutorial/Controller/CommandExplanationViewController.swift
@@ -13,6 +13,7 @@ class CommandExplanationViewController: UIViewController {
     var mainView: UIView!
     var backButton: UIButton!
     var nextViewButton: UIButton!
+    var textView1: UITextView!
     /*
     var textView1: UITextView!
     var textView2: UITextView!
@@ -47,13 +48,20 @@ class CommandExplanationViewController: UIViewController {
         nextViewButton.setTitleColor(.black, for: .normal)
         mainView.addSubview(nextViewButton)
         
+        //textView
+        textView1 = UITextView()
+        textView1.text = "Content of CommandExplanationView"
+        textView1.backgroundColor = .white
+        textView1.textColor = .black
+        mainView.addSubview(textView1)
+        
         /*
         //textView1
         textView1 = UITextView()
         textView1.text = "\"Es ist offensichtlich, dass wir für unsere Reise noch ein Schiff benötigen. Du siehst mir aus als könntst du ohne Probleme slebst eins bauen!\""
         textView1.backgroundColor = .white
-        textView1.textColor = .black
-        mainView.addSubview(textView1)
+         textView1.textColor = .black
+         mainView.addSubview(textView1)
         */
         //============adding constraints ============
         
@@ -63,6 +71,8 @@ class CommandExplanationViewController: UIViewController {
         mainView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
         mainView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
         mainView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1).isActive = true
+        
+        
         /*
         mainView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor)
         mainView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
@@ -80,21 +90,32 @@ class CommandExplanationViewController: UIViewController {
         backButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         backButton.leftAnchor.constraintEqualToSystemSpacingAfter(mainView.leftAnchor, multiplier: 0.5).isActive = true//ToDo: doesnt work
         
+        
+        //####### text view 1 ######
+       // textView1.translatesAutoresizingMaskIntoConstraints = false
+      //  textView1.topAnchor.constraint(equalTo: view.topAnchor, constant: 80).isActive = true
+     //
+        textView1.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        
         //###### next view button #####
         nextViewButton.translatesAutoresizingMaskIntoConstraints = false
         nextViewButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
         nextViewButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        nextViewButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 80).isActive = true
+        nextViewButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 170).isActive = true
         nextViewButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         
-        /*
+        
         //####### text view 1 ######
         textView1.translatesAutoresizingMaskIntoConstraints = false
         textView1.widthAnchor.constraint(equalToConstant: 200).isActive = true
         textView1.heightAnchor.constraint(equalToConstant: 80).isActive = true
-        textView1.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        textView1.topAnchor.constraint(equalTo: view.topAnchor, constant: 80).isActive = true
         nextViewButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 80).isActive = true
-        */
+        
+        textView1.isScrollEnabled = false
+        textView1.isEditable = false
+        textView1.isSelectable = false
+ 
         //============ adding logic ============
         
         //adds back to StartTutorial function to the back button
